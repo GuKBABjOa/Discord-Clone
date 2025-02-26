d0671884bf85e3542c507c57a3988c12e6cca2db
진성일 <wls9804@naver.com>
2025-02-26 17:33:28 +0900

    feat: db

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 const font = Open_Sans({ subsets: ["latin"] });
 
 
@@ -20,9 +21,17 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body
-          className={font.className}
+          className={cn(font.className,
+            "bg-white dark:bg-[#313338]"
+          )}
         >
-          {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            storageKey="discore-theme">
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
