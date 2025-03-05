493e21756bc69fb0fbcf02bdbe84a7788ec050be
진성일 <wls9804@naver.com>
2025-03-05 17:49:45 +0900

    UploadThing setup

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -13,7 +13,12 @@ const handleAuth = () => {
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-
+    serverImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+        .middleware(() => handleAuth())
+        .onUploadComplete(() => { }),
+    messageFile: f(["image", "pdf"])
+        .middleware(() => handleAuth())
+        .onUploadComplete(() => { })
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,29 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
 const isPublicRoute = createRouteMatcher([
     '/sign-in(.*)',
     '/sign-up(.*)'
-])
+]);
 
 export default clerkMiddleware(async (auth, request) => {
-    if (!isPublicRoute(request)) {
-        await auth.protect()
+    // 요청 URL에서 pathname을 확인하여 '/api/uploadthing' 경로는 건너뛰기
+    if (request.nextUrl.pathname.startsWith('/api/uploadthing')) {
+        return NextResponse.next();
     }
-})
+
+    if (!isPublicRoute(request)) {
+        await auth.protect();
+    }
+
+    return NextResponse.next();
+});
 
 export const config = {
     matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
+        // Next.js 내부 파일 및 정적 파일들을 제외하고
         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
+        // API 및 trpc 라우트에 대해 적용
         '/(api|trpc)(.*)',
     ],
-}
+};
